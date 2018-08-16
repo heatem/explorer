72b07a7d99a9a8210e6f9d7383cbd9562d051f0d
Heather Mason <heathermason@Heathers-MacBook-Pro.local>
2018-08-15 18:58:09 -0700

    added test for diveLogViewModel, updated duration to minutes

--- ExplorerTests/ExplorerTests.swift
+++ ExplorerTests/ExplorerTests.swift
@@ -21,16 +21,39 @@ class ExplorerTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testInitDiveLogViewModel() {
+        var dateComponents = DateComponents()
+        dateComponents.year = 2017
+        dateComponents.month = 02
+        dateComponents.day = 28
+        dateComponents.timeZone = TimeZone(abbreviation: "PST")
+        dateComponents.hour = 13
+        dateComponents.minute = 38
+        
+        let userCalendar = Calendar.current
+        let date = userCalendar.date(from: dateComponents)
+        
+        let timeOut = date!.addingTimeInterval(1800)
+        
+        let timeOutDate = userCalendar.date(from: timeOutDateComponents)
+        
+        let diveLog = DiveLog(
+            diveNumber: 1,
+            date: date!,
+            depth: 12,
+            location: "North Shore, HI",
+            entryType: .boat,
+            waterType: .salt,
+            timeIn: date!,
+            timeOut: timeOut,
+            notes: "This is a test",
+            userFullName: "H Mason",
+            userID: "hm",
+            userIcon: "HM")
+        
+        let diveLogViewModel = DiveLogViewModel(diveLog: diveLog)
+        
+        XCTAssertEqual(diveLogViewModel.date, "Tuesday, February 28, 2017 at 1:38 PM")
+        XCTAssertEqual(diveLogViewModel.duration, 30)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
